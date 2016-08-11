c150e268251a92ab237fa0ac89dda236bab9c2c0
awanjila <abemuchikan@gmail.com>
2016-08-12 00:43:55 +0300

    Adding the Article Component

--- app.ts
+++ app.ts
@@ -29,3 +29,57 @@ addArticle(title: HTMLInputElement, link:HTMLInputElement): void{ console.log(`A
 }
 }
 bootstrap(RedditApp);
+@Component({
+	selector: 'reddit-article',
+	host:{
+	   class: 'row'
+	},
+    template:`
+    <div class="four wide column center aligned votes">
+      <div class="ui statistic">
+         <div class="value>
+         {{ votes }}
+         </div>
+         <div class="label">
+           Points
+         </div>
+       </div>
+     </div>
+     <div class="twelve wide column">
+     <a class="ui large header" href="{{ link }}">
+     {{ title }}
+     </a>
+     <ul class="ui big horizontal list voters">
+       <li class="item">
+       <a href (click)="voteUp()">
+         <i class="arrow up icon"></1>
+           upvote
+       </a>
+       </li>
+       <li class="item">
+       <a href (click)="voteDown()">
+         <i class="arrow down icon"></1>
+           downvote
+       </a>
+       </li>
+     </ul>
+     </div>
+})
+class ArticleComponent{
+	votes: number;
+	title: string;
+	link: string;
+
+constructor(){
+	this.title = 'Angular 2';
+	this.link ='http://angular.io';
+	this.votes=3;
+}
+ voteUp() {
+  this.votes +=1;
+ }
+
+ voteDown() {
+ this.votes-=1;
+ }
+}
